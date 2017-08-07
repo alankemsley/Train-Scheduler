83fc71660d526b67007cf84dfc6e10b78d70e0d8
Alan Kemsley <alanjkemsley@gmail.com>
2017-08-07 17:00:29 -0400

    Adds pop-up model for large screens, and slide-up model for mobile.

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
-  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
+  // Trigger pop-up window
+  $(".modal").modal();
+
+  //Trigger slide-up window
+  $(window).resize(function(){
+  	if ($(window).width() <= 600){
+  		// do something here
+      console.log("Small!");
+      $("#model").addClass("bottom-sheet");
+  	} else {
+      console.log("Big!");
+      $("#model").removeClass("bottom-sheet");
+    }
+  });
+
+  //Remove FAB pulse
+  $("#add-button").on("click", function() {
+    $("#add-button").children("a").removeClass("pulse");
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
