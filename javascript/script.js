d72d00ae65db93c2842932ccdee3e2d2f3b0ca07
Alan Kemsley <alanjkemsley@gmail.com>
2017-08-07 21:54:12 -0400

    Complete front-end.

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   // Trigger modal
   $(".modal").modal();
@@ -8,21 +8,17 @@ $(document).ready(function(){
     $("#add-button").children("a").removeClass("pulse");
   });
 
-  //Form Time Input
+  //First train form nput
   $('.timepicker').pickatime({
-  default: 'now', // Set default time: 'now', '1:30AM', '16:30'
-  fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
-  twelvehour: false, // Use AM/PM or 24-hour format
-  donetext: 'OK', // text for done-button
-  cleartext: 'Clear', // text for clear-button
-  canceltext: 'Cancel', // Text for cancel-button
-  autoclose: false, // automatic close timepicker
-  ampmclickable: true, // make AM PM clickable
-  aftershow: function(){} //Function for after opening timepicker
-});
-
-
-
+    default: 'now',
+    twelvehour: false,
+    donetext: 'OK',
+    cleartext: 'Clear',
+    canceltext: 'Cancel',
+    autoclose: false,
+    ampmclickable: true,
+    aftershow: function() {}
+  });
 
 
 
