fc23e28722b0c3bca805292a7a3508ec240fff21
Alan Kemsley <alanjkemsley@gmail.com>
2017-08-07 20:57:24 -0400

    Changes modal to pop-up only. Completes form input fields.

--- javascript/script.js
+++ javascript/script.js
@@ -3,22 +3,23 @@ $(document).ready(function(){
   // Trigger modal
   $(".modal").modal();
 
-  // Modal format
-	if ($(window).width() <= 600){
-		// Slide-up modal
-    $("#modal").addClass("bottom-sheet");
-    $("#modal").removeClass("modal-fixed-footer");
-	} else {
-    // Pop-up modal
-    $("#modal").removeClass("bottom-sheet");
-    $("#modal").addClass("modal-fixed-footer");
-  }
-
   //Remove FAB pulse
   $("#add-button").on("click", function() {
     $("#add-button").children("a").removeClass("pulse");
   });
 
+  //Form Time Input
+  $('.timepicker').pickatime({
+  default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+  fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+  twelvehour: false, // Use AM/PM or 24-hour format
+  donetext: 'OK', // text for done-button
+  cleartext: 'Clear', // text for clear-button
+  canceltext: 'Cancel', // Text for cancel-button
+  autoclose: false, // automatic close timepicker
+  ampmclickable: true, // make AM PM clickable
+  aftershow: function(){} //Function for after opening timepicker
+});
 
 
 
